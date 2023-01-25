21e411d8973ca6d47bcf5afe5300bccdf8ac810c
Isao Hayakawa <isao.a.hayakawa@gmail.com>
2023-01-24 20:40:44 -0800

    chore: eslint fixes

--- src/graphql/types/company.js
+++ src/graphql/types/company.js
@@ -1,39 +1,39 @@
 import {
-    GraphQLObjectType,
-    GraphQLString
-  } from 'graphql'
-  import { PriceHistoryType } from './priceHistory.js'
-  import { getPriceHistory } from '../utils.js'
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql'
+import { PriceHistoryType } from './priceHistory.js'
+import { getPriceHistory } from '../utils.js'
   
-  const CompanyType = new GraphQLObjectType({
-    name: 'Company',
-    description: 'Company',
-    fields: {
-      name: {
-        type: GraphQLString,
-        description: 'Company name',
-        resolve: (obj) => {
-          return obj.longName
-        },
-      },
-      ticker: {
-        type: GraphQLString,
-        description: 'Company ticker',
-        resolve: (obj) => {
-          return obj.symbol
-        },
-      },
-      priceHistory: {
-        type: PriceHistoryType,
-        description: '1 year daily price history',
-        resolve: (obj) => {
-          return getPriceHistory(obj.symbol)
-        },
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  description: 'Company',
+  fields: {
+    name: {
+      type: GraphQLString,
+      description: 'Company name',
+      resolve: (obj) => {
+        return obj.longName
       },
     },
-  })
+    ticker: {
+      type: GraphQLString,
+      description: 'Company ticker',
+      resolve: (obj) => {
+        return obj.symbol
+      },
+    },
+    priceHistory: {
+      type: PriceHistoryType,
+      description: '1 year daily price history',
+      resolve: (obj) => {
+        return getPriceHistory(obj.symbol)
+      },
+    },
+  },
+})
   
-  export {
-    CompanyType,
-  }
+export {
+  CompanyType,
+}
   
--- src/graphql/types/query.js
+++ src/graphql/types/query.js
@@ -1,29 +1,29 @@
 import {
-    GraphQLObjectType,
-    GraphQLString
-  } from 'graphql'
-  import { CompanyType } from './company.js'
-  import { getCompanyData } from '../utils.js'
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql'
+import { CompanyType } from './company.js'
+import { getCompanyData } from '../utils.js'
   
-  const QueryType = new GraphQLObjectType({
-    name: 'QueryType',
-    fields: {
-      company: {
-        type: CompanyType,
-        args: { 
-          ticker: {
-            type: GraphQLString,
-            description: 'Company ticker',
-          }
-        },
-        resolve: (_, args) => {
-          return getCompanyData(args.ticker)
+const QueryType = new GraphQLObjectType({
+  name: 'QueryType',
+  fields: {
+    company: {
+      type: CompanyType,
+      args: { 
+        ticker: {
+          type: GraphQLString,
+          description: 'Company ticker',
         }
+      },
+      resolve: (_, args) => {
+        return getCompanyData(args.ticker)
       }
     }
-  })
-  
-  export {
-    QueryType,
   }
+})
+  
+export {
+  QueryType,
+}
   
